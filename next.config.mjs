8b2ee20017862b20c8d2935ff9fcb2e19b9ec150
faizanmasood302 <faizanmasood302@gmail.com>
2025-02-08 18:31:17 +0500

    Updated next.config.js to allow remote images from Sanity

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
-  },
-}
-
-export default nextConfig
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          port: '',
+          pathname: '/images/**',
+        },
+      ],
+    },
+  }
+  
+  export default nextConfig
