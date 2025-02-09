5fc97dbbe7d5874e2e166c84172ce1acf0d0774f
faizanmasood302 <faizanmasood302@gmail.com>
2025-02-09 10:58:24 +0500

    new changes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,4 +7,6 @@ export const client = createClient({
   dataset:'production',
   token:'sk7lieGgmtxyRP2vYeN9c1mpcSWwMXtxEdhc1P9xUMiLtn29lvweu0e8iAmB6MAH3cuJAWCZRA1n77BhJ5X9Gs7QLE8nKTnNNrtmfTMGHuUFR0Ut6N0EeMw3QdwSCpWqo00rKgQxnTZ18LkEVraxhS002QarkAnRLAb9uDLJ3Sx4xuExO0vM',
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  apiVersion: "2025-02-09"
+
 })
